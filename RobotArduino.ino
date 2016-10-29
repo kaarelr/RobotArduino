ed9919bae8a13fc4659c3276e0045c3f4f2d4774
kaarelr <kaarel.reinvars@gmail.com>
2016-10-29 22:05:50 +0300

    Kauguse mõõtmine

--- RobotArduino.ino
+++ RobotArduino.ino
@@ -12,3 +12,17 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+int calculateDistance(int sensorPin){
+  float volts = analogRead(sensorPin)*0.00322265625;   // value from sensor * (5/1024) - if running 3.3.volts then change 5 to 3.3
+  float distance = 65*pow(volts, -1.10);          // worked out from graph 65 = theretical distance / (1/Volts)S - luckylarry.co.uk
+  Serial.println(distance);                       // print the distance
+  delay(100);          // arbitary wait time.
+  return distance;
+  }
+
+void sensorLogics(){
+
+ 
+}
+
