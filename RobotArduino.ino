c83ffba3c5e5e541667ed3f9f963e1c1538208fb
SaCoder <sass.tammer@gmail.com>
2016-10-30 17:23:25 +0200

    Update RobotArduino.ino

--- RobotArduino.ino
+++ RobotArduino.ino
@@ -1,6 +1,11 @@
 void setup() {
   // put your setup code here, to run once:
-
+pinMode             //Määrame, kas see on input või output
+pinMode
+pinMode
+  
+delay (5000);       //5s viivitust enne mootorite tööle minekut
+motors_forward();
 }
 
 void loop() {
